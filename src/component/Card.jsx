f045cdf6db7488cd979b96a0c9446d03b996909c
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-08 21:32:27 +0600

    Explore conditional rendering and conditional css class

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,15 +2,45 @@ import React from 'react';
 import TShirt from './TShirt';
 
 const Card = ({cart, handleRemoveFromCart}) => {
-    console.log(cart);
+    let message;
+    if(cart.length === 0){
+        message = <p className='bg-green-300 rounded-lg p-2 text-white text-center'>Please add some Product</p>
+    }
+    else{
+        message = <p className='bg-purple-700 rounded-xl p-2 text-white'>Wow great...successfully added product</p>
+    }
     return (
         <div className='p-4 border-2'>
-            <h1 className='text-3xl font-semibold text-center'>Order Summary: {cart.length}</h1>
+            
+            <h1 className='text-3xl font-semibold text-center mt-8'>Order Summary: {cart.length}</h1>
+            <br />
+
+            {/* conditional rendering-2 */}
+            {/* {cart.length > 2 ? <span>Buy another some product and get your offer</span> : <span></span>} */}
+
+            {message}
+
+            <div className='mb-5 mt-5'>
+                {
+                    cart.map(tshirt => <h1 className='text-xl font-bold' key={tshirt._id}>{tshirt.name} <button onClick={()=>handleRemoveFromCart(tshirt._id)}   className='px-2 py-1 bg-yellow-500 rounded-lg'>X</button></h1>)
+                }
+            </div>
             {
-                cart.map(tshirt => <h1 className='text-xl font-bold' key={tshirt._id}>{tshirt.name} <button onClick={()=>handleRemoveFromCart(tshirt._id)}   className='px-2 py-1 bg-yellow-500 rounded-lg'>X</button></h1>)
+                cart.length === 2 && <p className='p-2 bg-fuchsia-600 rounded-lg font-semibold text-xl'>Double bonanza!!</p>
             }
+            {/* {
+                cart.length === 3 || <h3>3 ta to hoilo na re bondhu</h3>
+            } */}
         </div>
     );
 };
 
 export default Card;
+
+/**
+ * CONDITIONAL RENDERING 
+ * 1. USE if or else if to set a variable that will contain an element, component
+ * 2. ternary operator: condition ? 'for tru' : 'false'
+ * 3. Logical &&: (if the condition is true then the next thing will be displayed)
+ * 4. Logical || (if the condition is false then the next thing will be displayed)
+ */
--- src/component/TShirt.jsx
+++ src/component/TShirt.jsx
@@ -6,7 +6,7 @@ const TShirt = ({tshirt, handleAddToCart}) => {
         <div className='border-2 rounded-lg p-2 grid justify-center text-center gap-3'>
             <img className='rounded-lg h-64' src={picture} alt="" />
             <h3 className='text-2xl'>{name}</h3>
-            <p className='font-semibold'>Price: {price}</p>
+            <p className='font-semibold'>Price: ${price}</p>
             <button onClick={() => handleAddToCart(tshirt)} className='bg-emerald-500 rounded-xl px-4 py-2 w-full h-12'>Buy Now</button>
         </div>
     );
